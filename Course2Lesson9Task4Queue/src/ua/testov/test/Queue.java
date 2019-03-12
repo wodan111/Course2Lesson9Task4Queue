0ca299108c5e20b944d72bfef5e1735303ef3462
Vadim <testov@test.ua>
2019-03-12 20:58:10 +0200

    .

---
+++ Course2Lesson9Task4Queue/src/ua/testov/test/Queue.java
@@ -0,0 +1,24 @@
+package ua.testov.test;
+
+import java.util.ArrayDeque;
+
+public class Queue {
+private ArrayDeque<String> q = new ArrayDeque<>();
+
+public Queue() {
+	super();
+	queue();
+}
+private void queue(){
+	q.add("Sheldon");
+	q.add("Leonard");
+	q.add("Volovitc");
+	q.add("Kutrapalli");
+	q.add("Penny");
+}
+public ArrayDeque<String> magicQueue(int cup) {
+	
+	
+	return q;
+}
+}
